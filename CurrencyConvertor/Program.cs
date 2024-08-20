f642f6be115906cd7441ca3bf1ea4c561743e2d6
Rada A <arada@microsoft.com>
2024-08-20 23:21:02 +0530

    Added sample input

--- CurrencyConvertor/Program.cs
+++ CurrencyConvertor/Program.cs
@@ -8,8 +8,8 @@ namespace CurrencyConverterApp
         {
             // Create an instance for converting USD to INR with a default exchange rate
             CurrencyConverter usdToInrConverter = new CurrencyConverter("USD", "INR", 74.85);
-            double amountInInr = usdToInrConverter.ConvertAmount(100); 
-            Console.WriteLine($"100 USD to INR using default rate: {amountInInr} INR");
+            double amountInInr = usdToInrConverter.ConvertAmount(15); 
+            Console.WriteLine($"15 USD to INR using default rate: {amountInInr} INR");
 
             // Perform conversion using a custom exchange rate
             amountInInr = usdToInrConverter.ConvertAmount(100, 75.00); 
@@ -18,8 +18,8 @@ namespace CurrencyConverterApp
             // Create an instance for converting EUR to USD with a default exchange rate
             CurrencyConverter eurToUsdConverter = new CurrencyConverter("EUR", "USD", 1.18);
             // Perform conversion using the default rate
-            double amountInUsd = eurToUsdConverter.ConvertAmount(50); 
-            Console.WriteLine($"50 EUR to USD using default rate: {amountInUsd} USD");
+            double amountInUsd = eurToUsdConverter.ConvertAmount(500); 
+            Console.WriteLine($"500 EUR to USD using default rate: {amountInUsd} USD");
 
             // Perform conversion using a custom exchange rate
             amountInUsd = eurToUsdConverter.ConvertAmount(50, 1.20); 
